2e8fc74557e7a8bcf7c8b7599e2ea242c01ee2a9
codeZ <codeZ@USER-no-MacBook-Air-4.local>
2016-06-17 02:03:06 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# PetsHuggytest0
+=======
+# testguigui
+>>>>>>> 9613f1cd58f772e811d02c45ea994a483eba7993
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def index #views/index.html.erbを表示させるというアクション
+  end
+end
