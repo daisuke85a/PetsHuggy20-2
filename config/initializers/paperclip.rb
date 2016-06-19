8563f4eb21e1d8f9711bd06adcdff603d745b760
codeZ <codeZ@USER-no-MacBook-Air-4.local>
2016-06-20 03:24:01 +0900

    edit paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 Paperclip::Attachment.default_options[:url] = ':petshuggytest0.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-ap-northeast-1.amazonaws.com'
