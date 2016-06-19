39ed64b38b70ce815036302bd01f73c88502a416
codeZ <codeZ@USER-no-MacBook-Air-4.local>
2016-06-20 02:52:55 +0900

    edit config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,11 @@ Rails.application.configure do
   #remember to change localhost to actual host
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # s3 heroku config
   config.paperclip_defaults = {
   storage: :s3,
+  s3_protocol: :https,
+  s3_host_name: :ENV.fetch['S3_HOST_NAME'],
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
