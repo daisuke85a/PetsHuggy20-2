8532474284bc2fe67a28b188260af810671c6e06
codeZ <codeZ@USER-no-MacBook-Air-4.local>
2016-06-20 13:23:45 +0900

    s3 protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,7 @@ Rails.application.configure do
 
   # s3 heroku config
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_protocol: :https,  
+  storage: :s3,  
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
