263fefd94f0de3de0949f6bb10fcd582736e0211
CodeZtest0 <codeztest0@gmail.com>
2016-07-25 16:33:36 +0900

    Add check to Listing vertical navbar

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,6 +8,13 @@ class ListingsController < ApplicationController
 
   def show
     @photos = @listing.photos
+
+    # 今のユーザーがこのリスティングを予約しているか否か
+    @currentUserBooking = Reservation.where("listing_id = ? AND user_id = ?",@listing.id,current_user.id).present? if current_user
+    
+    @reviews = @listing.reviews
+    
+    @currentUserReview = @reviews.find_by(user_id:  current_user.id) if current_user
   end
 
   def new
@@ -61,6 +68,12 @@ class ListingsController < ApplicationController
   end
 
   def publish
+  end
+
+  def not_checked
+    @listing = Listing.find(params[:listing_id])
+    @listing.update(not_checked: params[:not_checked])
+    render :nothing => true
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714222655) do
+ActiveRecord::Schema.define(version: 20160725070620) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "home_type"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160714222655) do
     t.datetime "updated_at",      null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "not_checked"
   end
 
   add_index "listings", ["user_id"], name: "index_listings_on_user_id"
@@ -58,6 +59,18 @@ ActiveRecord::Schema.define(version: 20160714222655) do
 
   add_index "reservations", ["listing_id"], name: "index_reservations_on_listing_id"
   add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "rate"
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "reviews", ["listing_id"], name: "index_reviews_on_listing_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
